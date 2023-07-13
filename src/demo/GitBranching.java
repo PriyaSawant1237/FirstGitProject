036f233462a925a2cd78fa61acbd7d7e92407b26
priyasawant <priya.ctspl@gmail.com>
2023-07-13 16:22:35 +0530

    added new branch

---
+++ src/demo/GitBranching.java
@@ -0,0 +1,14 @@
+package demo;
+
+import java.util.ArrayList;
+
+public class GitBranching {
+public static void main(String[] args) {
+	ArrayList al=new ArrayList();
+	al.add(null);
+	al.add("pune");
+	al.add(50);
+	al.add(null);
+	System.out.println(al);
+}
+}
